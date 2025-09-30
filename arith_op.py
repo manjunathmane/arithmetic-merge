5f38e241715bc95c31774f7c014a83e2b67d87ba
manjunathmane <manjunathmane38@gmail.com>
2025-09-30 10:37:54 +0530

    Create arith_op.py

---
+++ arith_op.py
@@ -0,0 +1,14 @@
+#program for performing arithmetic operations on 2 numbers
+# Get user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform calculations
+sum_result = num1 + num2
+subtraction_result = num1 - num2
+
+# Display results
+print("Sum:", sum_result)
+print("Subtraction:", subtraction_result)
+
+
