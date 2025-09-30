1772a483958c04d7013d905dfb6c4e57e4b8a842
manjunathmane <manjunathmane38@gmail.com>
2025-09-30 10:39:51 +0530

    SAVE TO LOCAL

--- arith_op.py
+++ arith_op.py
@@ -11,4 +11,14 @@ subtraction_result = num1 - num2
 print("Sum:", sum_result)
 print("Subtraction:", subtraction_result)
 
+# Multiplication and division after sum and subtraction
+multiplication_result = num1 * num2
 
+if num2 != 0:
+    division_result = num1 / num2
+else:
+    division_result = "Undefined (cannot divide by zero)"
+
+#print output for multiplication and division
+print("Multiplication:", multiplication_result)
+print("Division:", division_result)
